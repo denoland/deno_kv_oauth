6ef980556f3b8ecddec432d8bd9a1c5e3437b293
Asher Gomez <ashersaupingomez@gmail.com>
2023-05-27 13:15:47 +1000

    refactor: functional approach (#2)
    * refactor: functional approach
    
    * lock

--- examples/fresh/routes/signin.ts
+++ examples/fresh/routes/signin.ts
@@ -1,9 +1,10 @@
 import { Handlers } from "$fresh/server.ts";
+import { signIn } from "deno_kv_oauth";
 import { State } from "./_middleware.ts";
 
 // deno-lint-ignore no-explicit-any
 export const handler: Handlers<any, State> = {
   async GET(_req, ctx) {
-    return await ctx.state.kvOAuthClient.signIn();
+    return await signIn(ctx.state.provider);
   },
 };
