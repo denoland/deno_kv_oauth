3a7339372754b2b68041ac78ad0870740b67fad8
Asher Gomez <ashersaupingomez@gmail.com>
2023-05-29 16:13:41 +1000

    refactor: simplify Fresh example (#5)

--- examples/fresh/fresh.gen.ts
+++ examples/fresh/fresh.gen.ts
@@ -3,19 +3,17 @@
 // This file is automatically updated during development when running `dev.ts`.
 
 import config from "./deno.json" assert { type: "json" };
-import * as $0 from "./routes/_middleware.ts";
-import * as $1 from "./routes/callback.ts";
-import * as $2 from "./routes/index.tsx";
-import * as $3 from "./routes/signin.ts";
-import * as $4 from "./routes/signout.ts";
+import * as $0 from "./routes/callback.ts";
+import * as $1 from "./routes/index.tsx";
+import * as $2 from "./routes/signin.ts";
+import * as $3 from "./routes/signout.ts";
 
 const manifest = {
   routes: {
-    "./routes/_middleware.ts": $0,
-    "./routes/callback.ts": $1,
-    "./routes/index.tsx": $2,
-    "./routes/signin.ts": $3,
-    "./routes/signout.ts": $4,
+    "./routes/callback.ts": $0,
+    "./routes/index.tsx": $1,
+    "./routes/signin.ts": $2,
+    "./routes/signout.ts": $3,
   },
   islands: {},
   baseUrl: import.meta.url,
--- examples/fresh/routes/signin.ts
+++ examples/fresh/routes/signin.ts
@@ -1,10 +1,9 @@
 import { Handlers } from "$fresh/server.ts";
 import { signIn } from "deno_kv_oauth";
-import { State } from "./_middleware.ts";
+import { provider } from "@/utils/provider.ts";
 
-// deno-lint-ignore no-explicit-any
-export const handler: Handlers<any, State> = {
-  async GET(_req, ctx) {
-    return await signIn(ctx.state.provider);
+export const handler: Handlers = {
+  async GET(_req) {
+    return await signIn(provider);
   },
 };
