e07104104063ecbad1729c019faee16a88920f99
Asher Gomez <ashersaupingomez@gmail.com>
2023-05-31 13:48:21 +1000

    refactor: remove Fresh demo (#23)
    * refactor: remove Fresh demo
    
    * further simplifications

--- examples/fresh/routes/signin.ts
+++
@@ -1,9 +0,0 @@
-import { Handlers } from "$fresh/server.ts";
-import { signIn } from "deno_kv_oauth";
-import { provider } from "@/utils/provider.ts";
-
-export const handler: Handlers = {
-  async GET(_req) {
-    return await signIn(provider);
-  },
-};
