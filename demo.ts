7c255d7b0098156493baf8f95ace8d83397c6ba0
Asher Gomez <ashersaupingomez@gmail.com>
2023-06-02 09:10:06 +1000

    refactor: demo to improve provider onboarding

--- demo.ts
+++ demo.ts
@@ -9,44 +9,40 @@ import { loadSync, serve, Status } from "./deps.ts";
 
 loadSync({ export: true });
 
+async function indexHandler(request: Request) {
+  let body = `
+    <p>Who are you?</p>
+    <a href="/signin">Sign in</a>
+  `;
+  if (isSignedIn(request)) {
+    const tokens = await getSessionTokens(request);
+    body = `
+      <p>Your tokens:<p>
+      <pre>${JSON.stringify(tokens, undefined, 2)}</pre>
+      <a href="/signout">Sign out</a>
+    `;
+  }
+  return new Response(body, {
+    headers: { "content-type": "text/html; charset=utf-8" },
+  });
+}
+
+const router: Record<string, (request: Request) => Promise<Response>> = {
+  "/": async (request) => await indexHandler(request),
+  "/signin": async (request) => await signIn(request, "github"),
+  "/callback": async (request) => await handleCallback(request, "github"),
+  "/signout": async (request) => await signOut(request),
+};
+
 async function handler(request: Request): Promise<Response> {
   if (request.method !== "GET") {
     return new Response(null, { status: Status.NotFound });
   }
 
   const { pathname } = new URL(request.url);
-
-  if (pathname === "/") {
-    let body = `
-      <p>Who are you?</p>
-      <a href="/signin">Sign in</a>
-    `;
-    if (isSignedIn(request)) {
-      const tokens = await getSessionTokens(request);
-      body = `
-        <p>Your tokens:<p>
-        <pre>${JSON.stringify(tokens, undefined, 2)}</pre>
-        <a href="/signout">Sign out</a>
-      `;
-    }
-    return new Response(body, {
-      headers: { "content-type": "text/html; charset=utf-8" },
-    });
-  }
-
-  if (pathname === "/signin") {
-    return await signIn(request, "github");
-  }
-
-  if (pathname === "/callback") {
-    return await handleCallback(request, "github");
-  }
-
-  if (pathname === "/signout") {
-    return signOut(request);
-  }
-
-  return new Response(null, { status: Status.NotFound });
+  const handler = router[pathname];
+  return await handler?.(request) ??
+    new Response(null, { status: Status.NotFound });
 }
 
 serve(handler);
