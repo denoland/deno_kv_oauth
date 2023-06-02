62d17033d8d7ad0a275e6f75f65fca48b845a47e
Asher Gomez <ashersaupingomez@gmail.com>
2023-06-03 09:48:18 +1000

    tweaks

--- src/create_client.ts
+++ src/create_client.ts
@@ -1,9 +1,13 @@
+import { assert } from "https://deno.land/std@0.189.0/testing/asserts.ts";
 import { OAuth2Client, OAuth2ClientConfig } from "../deps.ts";
 
 export type Provider = "github" | "discord";
 
-function createGitHubClientConfig(): OAuth2ClientConfig {
+function createGitHubClientConfig(
+  moreOAuth2ClientConfig?: OAuth2ClientConfig,
+): OAuth2ClientConfig {
   return {
+    ...moreOAuth2ClientConfig,
     clientId: Deno.env.get("GITHUB_CLIENT_ID")!,
     clientSecret: Deno.env.get("GITHUB_CLIENT_SECRET")!,
     authorizationEndpointUri: "https://github.com/login/oauth/authorize",
@@ -11,8 +15,15 @@ function createGitHubClientConfig(): OAuth2ClientConfig {
   };
 }
 
-function createDiscordClientConfig(): OAuth2ClientConfig {
+function createDiscordClientConfig(
+  moreOAuth2ClientConfig?: OAuth2ClientConfig,
+): OAuth2ClientConfig {
+  assert(
+    moreOAuth2ClientConfig?.defaults?.scope,
+    "OAuth2 scope is required for Discord client config",
+  );
   return {
+    ...moreOAuth2ClientConfig,
     clientId: Deno.env.get("DISCORD_CLIENT_ID")!,
     clientSecret: Deno.env.get("DISCORD_CLIENT_SECRET")!,
     authorizationEndpointUri: "https://discord.com/oauth2/authorize",
@@ -20,12 +31,17 @@ function createDiscordClientConfig(): OAuth2ClientConfig {
   };
 }
 
-export function createClient(provider: Provider): OAuth2Client {
+export function createClient(
+  provider: Provider,
+  moreOAuth2ClientConfig?: OAuth2ClientConfig,
+): OAuth2Client {
   switch (provider) {
     case "github":
-      return new OAuth2Client(createGitHubClientConfig());
+      return new OAuth2Client(createGitHubClientConfig(moreOAuth2ClientConfig));
     case "discord":
-      return new OAuth2Client(createDiscordClientConfig());
+      return new OAuth2Client(
+        createDiscordClientConfig(moreOAuth2ClientConfig),
+      );
     default:
       throw new Error(`Provider ID "${provider}" not supported`);
   }
