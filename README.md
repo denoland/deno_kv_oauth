82c214826ba847f05775600d419fd2eec350949c
Asher Gomez <ashersaupingomez@gmail.com>
2023-06-05 09:26:34 +1000

    work

--- README.md
+++ README.md
@@ -11,6 +11,68 @@ Minimal OAuth powered by Deno KV.
 
 ## Getting Started
 
+```ts
+// deno run --unstable --allow-env --allow-net demo.ts
+import {
+  createClient,
+  getSessionTokens,
+  handleCallback,
+  isSignedIn,
+  signIn,
+  signOut,
+} from "https://deno.land/x/deno_kv_oauth@v0.1.2-beta/mod.ts";
+import { serve, Status } from "https://deno.land/std/http/mod.ts";
+
+const client = createClient("github");
+
+async function indexHandler(request: Request) {
+  let body = `
+    <p>Who are you?</p>
+    <p><a href="/signin">Sign in with GitHub</a></p>
+  `;
+  if (isSignedIn(request)) {
+    const tokens = await getSessionTokens(request);
+    body = `
+      <p>Your tokens:<p>
+      <pre>${JSON.stringify(tokens, undefined, 2)}</pre>
+      <a href="/signout">Sign out</a>
+    `;
+  }
+  return new Response(body, {
+    headers: { "content-type": "text/html; charset=utf-8" },
+  });
+}
+
+async function handler(request: Request): Promise<Response> {
+  if (request.method !== "GET") {
+    return new Response(null, { status: Status.NotFound });
+  }
+
+  const { pathname } = new URL(request.url);
+  switch (pathname) {
+    case "/": {
+      return await indexHandler(request);
+    }
+    case "/signin": {
+      return await signIn(request, client);
+    }
+    case "/callback": {
+      return await handleCallback(request, client);
+    }
+    case "/signout": {
+      return await signOut(request);
+    }
+    default: {
+      return new Response(null, { status: Status.NotFound });
+    }
+  }
+}
+
+serve(handler);
+```
+
+You can also check out a live demo at https://kv-oauth.deno.com.
+
 ## Contributing
 
 Before submitting a pull request, please run `deno task ok` and ensure all
--- demo.ts
+++ demo.ts
@@ -11,22 +11,12 @@ import { loadSync, serve, Status } from "./deps.ts";
 
 loadSync({ export: true });
 
-const redirectUriBase = Deno.env.get("DENO_DEPLOYMENT_ID")
-  ? "https://kv-oauth.deno.dev"
-  : "http://localhost:8000";
-const githubclient = createClient("github", {
-  redirectUri: redirectUriBase + "/callback/github",
-});
-const discordClient = createClient("discord", {
-  redirectUri: redirectUriBase + "/callback/discord",
-  defaults: { scope: "identify" },
-});
+const client = createClient("github");
 
 async function indexHandler(request: Request) {
   let body = `
     <p>Who are you?</p>
-    <p><a href="/signin/discord">Sign in with Discord</a></p>
-    <p><a href="/signin/github">Sign in with GitHub</a></p>
+    <p><a href="/signin">Sign in with GitHub</a></p>
   `;
   if (isSignedIn(request)) {
     const tokens = await getSessionTokens(request);
@@ -41,24 +31,29 @@ async function indexHandler(request: Request) {
   });
 }
 
-const router: Record<string, (request: Request) => Promise<Response>> = {
-  "/": (request) => indexHandler(request),
-  "/signin/discord": (request) => signIn(request, discordClient, "identify"),
-  "/callback/discord": (request) => handleCallback(request, discordClient),
-  "/signin/github": (request) => signIn(request, githubclient),
-  "/callback/github": (request) => handleCallback(request, githubclient),
-  "/signout": (request) => signOut(request),
-};
-
 async function handler(request: Request): Promise<Response> {
   if (request.method !== "GET") {
     return new Response(null, { status: Status.NotFound });
   }
 
   const { pathname } = new URL(request.url);
-  const handler = router[pathname];
-  return await handler?.(request) ??
-    new Response(null, { status: Status.NotFound });
+  switch (pathname) {
+    case "/": {
+      return await indexHandler(request);
+    }
+    case "/signin": {
+      return await signIn(request, client);
+    }
+    case "/callback": {
+      return await handleCallback(request, client);
+    }
+    case "/signout": {
+      return await signOut(request);
+    }
+    default: {
+      return new Response(null, { status: Status.NotFound });
+    }
+  }
 }
 
 serve(handler);
