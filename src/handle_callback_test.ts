3ddc8b0147e4ffde6e3d246a99769292d5d2acba
Asher Gomez <ashersaupingomez@gmail.com>
2023-06-06 12:34:51 +1000

    chore: add `handleCallback()` tests

---
+++ src/handle_callback_test.ts
@@ -0,0 +1,45 @@
+// Copyright 2023 the Deno authors. All rights reserved. MIT license.
+import { handleCallback } from "./handle_callback.ts";
+import { assertRejects, OAuth2Client } from "../deps.ts";
+import {
+  OAUTH_COOKIE_NAME,
+  type OAuthSession,
+  setOAuthSession,
+} from "./_core.ts";
+
+Deno.test("handleCallback()", async (test) => {
+  const client = new OAuth2Client({
+    clientId: crypto.randomUUID(),
+    clientSecret: crypto.randomUUID(),
+    authorizationEndpointUri: "https://example.com/authorize",
+    tokenUri: "https://example.com/token",
+  });
+
+  await test.step("no oauth cookie", async () => {
+    const request = new Request("http://example.com");
+    await assertRejects(() => handleCallback(request, client));
+  });
+
+  await test.step("oauth cookie with invalid value", async () => {
+    const request = new Request("http://example.com", {
+      headers: { cookie: `${OAUTH_COOKIE_NAME}=xxx` },
+    });
+    await assertRejects(() => handleCallback(request, client));
+  });
+
+  await test.step("rejects at furthest point", async () => {
+    const oauthSessionId = crypto.randomUUID();
+    const oauthSession: OAuthSession = {
+      state: crypto.randomUUID(),
+      codeVerifier: crypto.randomUUID(),
+    };
+    await setOAuthSession(oauthSessionId, oauthSession);
+    const request = new Request(
+      "http://example.com/callback?code=xxx&state=xxx",
+      {
+        headers: { cookie: `${OAUTH_COOKIE_NAME}=${oauthSessionId}` },
+      },
+    );
+    await assertRejects(async () => await handleCallback(request, client));
+  });
+});
