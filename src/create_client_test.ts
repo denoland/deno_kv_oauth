fb0a8690966d79a266708e2eb3645d8ce738c0d5
Lino Le Van <11367844+lino-levan@users.noreply.github.com>
2023-06-06 15:11:21 -0700

    feat: gitlab client (#57)
    * feat: gitlab client
    
    * nit
    
    ---------
    
    Co-authored-by: Asher Gomez <ashersaupingomez@gmail.com>

--- src/create_client_test.ts
+++ src/create_client_test.ts
@@ -52,6 +52,34 @@ Deno.test("createClient()", async (test) => {
     assertEquals(client.config.defaults, defaults);
   });
 
+  await test.step("gitlab", () => {
+    assertThrows(() => createClient("gitlab"));
+    assertThrows(() =>
+      createClient("gitlab", {
+        defaults: { scope: "scope_without_redirect_url" },
+      })
+    );
+    assertThrows(() =>
+      createClient("gitlab", {
+        redirectUri: "http://redirect-without-scope.com",
+      })
+    );
+
+    const clientId = crypto.randomUUID();
+    const clientSecret = crypto.randomUUID();
+    const redirectUri = "http://example.com";
+    const defaults = { scope: "scope" };
+
+    Deno.env.set("GITLAB_CLIENT_ID", clientId);
+    Deno.env.set("GITLAB_CLIENT_SECRET", clientSecret);
+
+    const client = createClient("gitlab", { redirectUri, defaults });
+    assertEquals(client.config.clientId, clientId);
+    assertEquals(client.config.clientSecret, clientSecret);
+    assertEquals(client.config.redirectUri, redirectUri);
+    assertEquals(client.config.defaults, defaults);
+  });
+
   await test.step("google", () => {
     assertThrows(() => createClient("google"));
     assertThrows(() =>
