b7a8f00252910bc5f8ebf6772fb488b14ff8e2d5
Asher Gomez <ashersaupingomez@gmail.com>
2023-06-10 15:06:21 +1000

    refactor: smarter tests

--- src/get_session_id_test.ts
+++ src/get_session_id_test.ts
@@ -1,24 +1,34 @@
 // Copyright 2023 the Deno authors. All rights reserved. MIT license.
 import { assertEquals } from "../deps.ts";
-import { SITE_COOKIE_NAME } from "./_core.ts";
+import { setTokensBySiteSession, SITE_COOKIE_NAME } from "./_core.ts";
 import { getSessionId } from "./get_session_id.ts";
 
-Deno.test("await getSessionId()", async () => {
-  const insecureRequest = new Request("http://example.com");
-  assertEquals(await getSessionId(insecureRequest), null);
+Deno.test("getSessionId()", async (test) => {
+  await test.step("returns null for invalid cookie", async () => {
+    const request = new Request("http://example.com");
+    assertEquals(await getSessionId(request), null);
+  });
 
-  insecureRequest.headers.set("cookie", "not-site-session=xxx");
-  assertEquals(await getSessionId(insecureRequest), null);
+  await test.step("returns null for non-existent session ID", async () => {
+    const request = new Request("http://example.com", {
+      headers: {
+        cookie: `${SITE_COOKIE_NAME}=nil`,
+      },
+    });
+    assertEquals(await getSessionId(request), null);
+  });
 
-  insecureRequest.headers.set("cookie", `${SITE_COOKIE_NAME}=xxx`);
-  assertEquals(await getSessionId(insecureRequest), null);
-
-  const secureRequest = new Request("https://example.com");
-  assertEquals(await getSessionId(secureRequest), null);
-
-  secureRequest.headers.set("cookie", "not-site-session=xxx");
-  assertEquals(await getSessionId(secureRequest), null);
-
-  secureRequest.headers.set("cookie", `__Host-${SITE_COOKIE_NAME}=xxx`);
-  assertEquals(await getSessionId(secureRequest), null);
+  await test.step("returns existent session ID", async () => {
+    const sessionId = crypto.randomUUID();
+    await setTokensBySiteSession(sessionId, {
+      accessToken: crypto.randomUUID(),
+      tokenType: crypto.randomUUID(),
+    });
+    const request = new Request("http://example.com", {
+      headers: {
+        cookie: `${SITE_COOKIE_NAME}=${sessionId}`,
+      },
+    });
+    assertEquals(await getSessionId(request), sessionId);
+  });
 });
--- src/handle_callback_test.ts
+++ src/handle_callback_test.ts
@@ -1,13 +1,7 @@
 // Copyright 2023 the Deno authors. All rights reserved. MIT license.
 import { handleCallback } from "./handle_callback.ts";
 import { assertRejects, OAuth2Client } from "../deps.ts";
-import {
-  getOAuthSession,
-  OAUTH_COOKIE_NAME,
-  type OAuthSession,
-  setOAuthSession,
-} from "./_core.ts";
-import { assertEquals } from "https://deno.land/std@0.190.0/testing/asserts.ts";
+import { OAUTH_COOKIE_NAME } from "./_core.ts";
 
 Deno.test("handleCallback()", async (test) => {
   const client = new OAuth2Client({
@@ -17,32 +11,15 @@ Deno.test("handleCallback()", async (test) => {
     tokenUri: "https://example.com/token",
   });
 
-  await test.step("no oauth cookie", async () => {
+  await test.step("rejects for no OAuth cookie", async () => {
     const request = new Request("http://example.com");
     await assertRejects(() => handleCallback(request, client));
   });
 
-  await test.step("no oauth session", async () => {
+  await test.step("rejects for non-existent OAuth session", async () => {
     const request = new Request("http://example.com", {
       headers: { cookie: `${OAUTH_COOKIE_NAME}=xxx` },
     });
     await assertRejects(() => handleCallback(request, client));
-  });
-
-  await test.step("rejects at furthest point", async () => {
-    const oauthSessionId = crypto.randomUUID();
-    const oauthSession: OAuthSession = {
-      state: crypto.randomUUID(),
-      codeVerifier: crypto.randomUUID(),
-    };
-    await setOAuthSession(oauthSessionId, oauthSession);
-    const request = new Request(
-      "http://example.com/callback?code=xxx&state=xxx",
-      {
-        headers: { cookie: `${OAUTH_COOKIE_NAME}=${oauthSessionId}` },
-      },
-    );
-    await assertRejects(async () => await handleCallback(request, client));
-    assertEquals(await getOAuthSession(oauthSessionId), null);
   });
 });
