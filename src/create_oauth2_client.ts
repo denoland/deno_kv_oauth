d646174e51ea071b5f59fe83dd500c80735806f4
j3lte <jwlagendijk@gmail.com>
2023-06-16 16:42:16 +0200

    feat: facebook client

--- src/create_oauth2_client.ts
+++ src/create_oauth2_client.ts
@@ -72,6 +72,43 @@ export function createGitHubOAuth2Client(
 }
 
 /**
+ * Creates an OAuth 2.0 client with Facebook as the provider.
+ *
+ * Requires `--allow-env[=GITHUB_CLIENT_ID,GITHUB_CLIENT_SECRET]` permissions and environment variables:
+ * 1. `FACEBOOK_CLIENT_ID`
+ * 2. `FACEBOOK_CLIENT_SECRET`
+ *
+ * @example
+ * ```ts
+ * import { createFacebookOAuth2Client } from "https://deno.land/x/deno_kv_oauth/mod.ts";
+ *
+ * const oauth2Client = createFacebookOAuth2Client({
+ *   redirectUri: "http://localhost:8000/callback",
+ *   defaults: {
+ *    scope: "email"
+ *   }
+ * });
+ * ```
+ *
+ * @see {@link https://developers.facebook.com/docs/facebook-login/guides/advanced/manual-flow}
+ */
+export function createFacebookOAuth2Client(
+  additionalOAuth2ClientConfig:
+  & Partial<OAuth2ClientConfig>
+  & WithScope
+  & WithRedirectUri,
+): OAuth2Client {
+  return new OAuth2Client({
+    clientId: Deno.env.get("FACEBOOK_CLIENT_ID")!,
+    clientSecret: Deno.env.get("FACEBOOK_CLIENT_SECRET")!,
+    authorizationEndpointUri: "https://www.facebook.com/v17.0/dialog/oauth",
+    tokenUri: "https://graph.facebook.com/v17.0/oauth/access_token",
+    ...additionalOAuth2ClientConfig,
+  });
+}
+
+
+/**
  * Creates an OAuth 2.0 client with GitLab as the provider.
  *
  * Requires `--allow-env[=GITLAB_CLIENT_ID,GITLAB_CLIENT_SECRET]` permissions and environment variables:
