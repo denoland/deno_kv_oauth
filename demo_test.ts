14833d7f6dcfa22512f2fa2043e13ab60141213f
Asher Gomez <ashersaupingomez@gmail.com>
2023-06-18 16:19:30 +1000

    work

--- demo_test.ts
+++ demo_test.ts
@@ -3,6 +3,7 @@ import { assertStringIncludes } from "https://deno.land/std@0.191.0/testing/asse
 import { handler } from "./demo.ts";
 import { assert, assertEquals, delay } from "./dev_deps.ts";
 import { Status } from "./deps.ts";
+import { setTokensBySession, SITE_COOKIE_NAME } from "./src/_core.ts";
 
 const port = 8001;
 Deno.serve({ port }, handler);
@@ -10,7 +11,49 @@ Deno.serve({ port }, handler);
 Deno.test("demo", async (test) => {
   await delay(100);
 
-  await test.step("/ route returns a basic web page", async () => {
+  await test.step("non-GET request methods returns not found response", async () => {
+    const response = await fetch(`http://localhost:${port}`, {
+      method: "POST",
+    });
+    await response.body?.cancel();
+
+    assert(!response.ok);
+    assertEquals(response.status, Status.NotFound);
+  });
+
+  await test.step("non-existent path returns not found response", async () => {
+    const response = await fetch(`http://localhost:${port}/nil`);
+    await response.body?.cancel();
+
+    assert(!response.ok);
+    assertEquals(response.status, Status.NotFound);
+  });
+
+  await test.step("/ route returns a basic web page when signed in", async () => {
+    const sessionId = crypto.randomUUID();
+    const accessToken = crypto.randomUUID();
+    await setTokensBySession(sessionId, {
+      accessToken,
+      tokenType: crypto.randomUUID(),
+    });
+    const response = await fetch(`http://localhost:${port}`, {
+      headers: {
+        cookie: `${SITE_COOKIE_NAME}=${sessionId}`,
+      },
+    });
+    const html = await response.text();
+
+    assert(response.ok);
+    assertEquals(response.status, 200);
+    assertEquals(
+      response.headers.get("content-type"),
+      "text/html; charset=utf-8",
+    );
+    assertStringIncludes(html, `Your access token: ${accessToken}`);
+    assertStringIncludes(html, '<a href="/signout">Sign out</a>');
+  });
+
+  await test.step("/ route returns a basic web page when signed out", async () => {
     const response = await fetch(`http://localhost:${port}`);
     const html = await response.text();
 
