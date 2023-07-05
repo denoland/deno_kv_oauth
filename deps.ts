9042ab3622977f290f7df22a4521e481fb223f39
iuioiua <iuioiua@users.noreply.github.com>
2023-07-05 00:11:13 +0000

    [create-pull-request] automated change

--- deps.ts
+++ deps.ts
@@ -3,18 +3,18 @@ export {
   isRedirectStatus,
   type RedirectStatus,
   Status,
-} from "https://deno.land/std@0.192.0/http/http_status.ts";
+} from "https://deno.land/std@0.193.0/http/http_status.ts";
 export {
   type Cookie,
   deleteCookie,
   getCookies,
   getSetCookies,
   setCookie,
-} from "https://deno.land/std@0.192.0/http/cookie.ts";
+} from "https://deno.land/std@0.193.0/http/cookie.ts";
 export {
   OAuth2Client,
   type OAuth2ClientConfig,
   OAuth2ResponseError,
   type Tokens,
 } from "https://deno.land/x/oauth2_client@v1.0.2/mod.ts";
-export { SECOND } from "https://deno.land/std@0.192.0/datetime/constants.ts";
+export { SECOND } from "https://deno.land/std@0.193.0/datetime/constants.ts";
