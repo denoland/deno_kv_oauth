c314c0613be5159140c2694a5aaa86c641f0a365
Asher Gomez <ashersaupingomez@gmail.com>
2023-07-19 12:46:01 +1000

    refactor: use `Deno.serve()`

--- dev_deps.ts
+++ dev_deps.ts
@@ -9,6 +9,5 @@ export {
 } from "https://deno.land/std@0.194.0/testing/asserts.ts";
 export { walk } from "https://deno.land/std@0.194.0/fs/walk.ts";
 export { globToRegExp } from "https://deno.land/std@0.194.0/path/glob.ts";
-export { serve } from "https://deno.land/std@0.194.0/http/server.ts";
 export { loadSync } from "https://deno.land/std@0.194.0/dotenv/mod.ts";
 export * from "./deps.ts";
