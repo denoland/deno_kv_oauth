f4175dba0776a6530f4d03800a12d26096ab01ee
Asher Gomez <ashersaupingomez@gmail.com>
2023-07-20 11:23:16 +1000

    chore: migrate remaining asserts

--- dev_deps.ts
+++ dev_deps.ts
@@ -6,7 +6,7 @@ export {
   assertRejects,
   assertStringIncludes,
   assertThrows,
-} from "https://deno.land/std@0.195.0/testing/asserts.ts";
+} from "https://deno.land/std@0.195.0/assert/mod.ts";
 export { walk } from "https://deno.land/std@0.195.0/fs/walk.ts";
 export { globToRegExp } from "https://deno.land/std@0.195.0/path/glob.ts";
 export { loadSync } from "https://deno.land/std@0.195.0/dotenv/mod.ts";
