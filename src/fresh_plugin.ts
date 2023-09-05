e7e5001864cc72afa8d7f48a49efb9012d584457
Asher Gomez <ashersaupingomez@gmail.com>
2023-09-05 10:35:22 +1000

    docs: correct Fresh plugin examples

--- src/fresh_plugin.ts
+++ src/fresh_plugin.ts
@@ -16,10 +16,8 @@ import { signOut } from "./sign_out.ts";
  * ```ts
  * // main.ts
  * import { start } from "$fresh/server.ts";
- * import {
- *   kvOAuthPlugin,
- *   createGitHubOAuth2Client
- * } from "https://deno.land/x/deno_kv_oauth@$VERSION/fresh.ts";
+ * import { createGitHubOAuth2Client } from "https://deno.land/x/deno_kv_oauth@$VERSION/mod.ts";
+ * import { kvOAuthPlugin } from "https://deno.land/x/deno_kv_oauth@$VERSION/fresh.ts";
  * import manifest from "./fresh.gen.ts";
  *
  * await start(manifest, {
