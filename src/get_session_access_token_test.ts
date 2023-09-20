bb4b7347405fa85c531dadbbc7aa977ec6d09f98
Asher Gomez <ashersaupingomez@gmail.com>
2023-09-20 22:05:18 +1000

    chore: flatten tests

--- src/get_session_access_token_test.ts
+++ src/get_session_access_token_test.ts
@@ -4,74 +4,72 @@ import { assertEquals, assertRejects, type Tokens } from "../dev_deps.ts";
 import { setTokens } from "./core.ts";
 import { randomOAuthConfig, randomTokens } from "./test_utils.ts";
 
-Deno.test("getSessionAccessToken()", async (test) => {
-  await test.step("returns null for non-existent session", async () => {
-    assertEquals(await getSessionAccessToken(randomOAuthConfig(), "nil"), null);
-  });
+Deno.test("getSessionAccessToken() returns null for non-existent session", async () => {
+  assertEquals(await getSessionAccessToken(randomOAuthConfig(), "nil"), null);
+});
 
-  await test.step("returns the access token for session without refresh token", async () => {
-    const sessionId = crypto.randomUUID();
-    const tokens: Tokens = {
-      ...randomTokens(),
-      expiresIn: 3, // if we had a refresh token this would cause a refresh
-    };
-    await setTokens(sessionId, tokens);
-    assertEquals(
-      await getSessionAccessToken(randomOAuthConfig(), sessionId),
-      tokens.accessToken,
-    );
-  });
+Deno.test("getSessionAccessToken() returns the access token for session without refresh token", async () => {
+  const sessionId = crypto.randomUUID();
+  const tokens: Tokens = {
+    ...randomTokens(),
+    expiresIn: 3, // if we had a refresh token this would cause a refresh
+  };
+  await setTokens(sessionId, tokens);
+  assertEquals(
+    await getSessionAccessToken(randomOAuthConfig(), sessionId),
+    tokens.accessToken,
+  );
+});
 
-  await test.step("returns the access token for session without expiry", async () => {
-    const sessionId = crypto.randomUUID();
-    const tokens: Tokens = {
-      ...randomTokens(),
-      refreshToken: crypto.randomUUID(),
-    };
-    await setTokens(sessionId, tokens);
-    assertEquals(
-      await getSessionAccessToken(randomOAuthConfig(), sessionId),
-      tokens.accessToken,
-    );
-  });
+Deno.test("getSessionAccessToken() returns the access token for session without expiry", async () => {
+  const sessionId = crypto.randomUUID();
+  const tokens: Tokens = {
+    ...randomTokens(),
+    refreshToken: crypto.randomUUID(),
+  };
+  await setTokens(sessionId, tokens);
+  assertEquals(
+    await getSessionAccessToken(randomOAuthConfig(), sessionId),
+    tokens.accessToken,
+  );
+});
 
-  await test.step("returns the access token for session with far expiry", async () => {
-    const sessionId = crypto.randomUUID();
-    const tokens: Tokens = {
-      ...randomTokens(),
-      refreshToken: crypto.randomUUID(),
-      expiresIn: 30,
-    };
-    await setTokens(sessionId, tokens);
-    assertEquals(
-      await getSessionAccessToken(randomOAuthConfig(), sessionId),
-      tokens.accessToken,
-    );
-  });
+Deno.test("getSessionAccessToken() returns the access token for session with far expiry", async () => {
+  const sessionId = crypto.randomUUID();
+  const tokens: Tokens = {
+    ...randomTokens(),
+    refreshToken: crypto.randomUUID(),
+    expiresIn: 30,
+  };
+  await setTokens(sessionId, tokens);
+  assertEquals(
+    await getSessionAccessToken(randomOAuthConfig(), sessionId),
+    tokens.accessToken,
+  );
+});
 
-  await test.step("rejects for an access token which expires in less than 5 seconds", async () => {
-    const sessionId = crypto.randomUUID();
-    const tokens: Tokens = {
-      ...randomTokens(),
-      refreshToken: crypto.randomUUID(),
-      expiresIn: 3,
-    };
-    await setTokens(sessionId, tokens);
-    await assertRejects(async () =>
-      await getSessionAccessToken(randomOAuthConfig(), sessionId)
-    );
-  });
+Deno.test("getSessionAccessToken() rejects for an access token which expires in less than 5 seconds", async () => {
+  const sessionId = crypto.randomUUID();
+  const tokens: Tokens = {
+    ...randomTokens(),
+    refreshToken: crypto.randomUUID(),
+    expiresIn: 3,
+  };
+  await setTokens(sessionId, tokens);
+  await assertRejects(async () =>
+    await getSessionAccessToken(randomOAuthConfig(), sessionId)
+  );
+});
 
-  await test.step("rejects for an expired access token", async () => {
-    const sessionId = crypto.randomUUID();
-    const tokens: Tokens = {
-      ...randomTokens(),
-      refreshToken: crypto.randomUUID(),
-      expiresIn: 0,
-    };
-    await setTokens(sessionId, tokens);
-    await assertRejects(async () =>
-      await getSessionAccessToken(randomOAuthConfig(), sessionId)
-    );
-  });
+Deno.test("getSessionAccessToken() rejects for an expired access token", async () => {
+  const sessionId = crypto.randomUUID();
+  const tokens: Tokens = {
+    ...randomTokens(),
+    refreshToken: crypto.randomUUID(),
+    expiresIn: 0,
+  };
+  await setTokens(sessionId, tokens);
+  await assertRejects(async () =>
+    await getSessionAccessToken(randomOAuthConfig(), sessionId)
+  );
 });
--- src/handle_callback_test.ts
+++ src/handle_callback_test.ts
@@ -4,27 +4,25 @@ import { assertEquals, assertRejects } from "../dev_deps.ts";
 import { getOAuthSession, OAUTH_COOKIE_NAME, setOAuthSession } from "./core.ts";
 import { randomOAuthConfig, randomOAuthSession } from "./test_utils.ts";
 
-Deno.test("handleCallback()", async (test) => {
-  await test.step("rejects for no OAuth cookie", async () => {
-    const request = new Request("http://example.com");
-    await assertRejects(() => handleCallback(request, randomOAuthConfig()));
-  });
+Deno.test("handleCallback() rejects for no OAuth cookie", async () => {
+  const request = new Request("http://example.com");
+  await assertRejects(() => handleCallback(request, randomOAuthConfig()));
+});
 
-  await test.step("rejects for non-existent OAuth session", async () => {
-    const request = new Request("http://example.com", {
-      headers: { cookie: `${OAUTH_COOKIE_NAME}=xxx` },
-    });
-    await assertRejects(() => handleCallback(request, randomOAuthConfig()));
+Deno.test("handleCallback() rejects for non-existent OAuth session", async () => {
+  const request = new Request("http://example.com", {
+    headers: { cookie: `${OAUTH_COOKIE_NAME}=xxx` },
   });
+  await assertRejects(() => handleCallback(request, randomOAuthConfig()));
+});
 
-  await test.step("deletes the OAuth session KV entry", async () => {
-    const oauthSessionId = crypto.randomUUID();
-    const oauthSession = randomOAuthSession();
-    await setOAuthSession(oauthSessionId, oauthSession);
-    const request = new Request("http://example.com", {
-      headers: { cookie: `${OAUTH_COOKIE_NAME}=${oauthSessionId}` },
-    });
-    await assertRejects(() => handleCallback(request, randomOAuthConfig()));
-    await assertEquals(await getOAuthSession(oauthSessionId), null);
+Deno.test("handleCallback() deletes the OAuth session KV entry", async () => {
+  const oauthSessionId = crypto.randomUUID();
+  const oauthSession = randomOAuthSession();
+  await setOAuthSession(oauthSessionId, oauthSession);
+  const request = new Request("http://example.com", {
+    headers: { cookie: `${OAUTH_COOKIE_NAME}=${oauthSessionId}` },
   });
+  await assertRejects(() => handleCallback(request, randomOAuthConfig()));
+  await assertEquals(await getOAuthSession(oauthSessionId), null);
 });
