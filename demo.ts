c8be301fbf35baa604cf4dbb58ec612f9963d290
Asher Gomez <ashersaupingomez@gmail.com>
2023-09-20 19:56:09 +1000

    refactor: simplify demo

--- demo.ts
+++ demo.ts
@@ -1,19 +1,7 @@
 // Copyright 2023 the Deno authors. All rights reserved. MIT license.
 import { loadSync, Status } from "./dev_deps.ts";
 import {
-  createAuth0OAuthConfig,
-  createDiscordOAuthConfig,
-  createDropboxOAuthConfig,
-  createFacebookOAuthConfig,
   createGitHubOAuthConfig,
-  createGitLabOAuthConfig,
-  createGoogleOAuthConfig,
-  createNotionOAuthConfig,
-  createOktaOAuthConfig,
-  createPatreonOAuthConfig,
-  createSlackOAuthConfig,
-  createSpotifyOAuthConfig,
-  createTwitterOAuthConfig,
   getSessionAccessToken,
   getSessionId,
   handleCallback,
@@ -23,50 +11,17 @@ import {
 
 loadSync({ export: true });
 
-/** @todo(iuioiua) Simplify this demo and instead provide guidance on how to change the demo parameters. */
-
 /**
- * Allows for dynamic provider selection useful for testing.
- * In production, just use import and use the provider's OAuth configuration creator.
+ * Modify the OAuth configuration creation function when testing for providers.
  *
  * @example
  * ```ts
- * import { createGitHubOAuthConfig } from "https://deno.land/x/deno_kv_oauth@$VERSION/mod.ts";
+ * import { createNotionOAuthConfig } from "./mod.ts";
  *
- * const oauthConfig = createGitHubOAuthConfig();
+ * const oauthConfig = createNotionOAuthConfig();
  * ```
  */
-const provider = Deno.env.get("PROVIDER") ?? "GitHub";
-const createOAuthConfigFn = {
-  Auth0: createAuth0OAuthConfig,
-  Discord: createDiscordOAuthConfig,
-  Dropbox: createDropboxOAuthConfig,
-  Facebook: createFacebookOAuthConfig,
-  GitHub: createGitHubOAuthConfig,
-  GitLab: createGitLabOAuthConfig,
-  Google: createGoogleOAuthConfig,
-  Notion: createNotionOAuthConfig,
-  Okta: createOktaOAuthConfig,
-  Patreon: createPatreonOAuthConfig,
-  Slack: createSlackOAuthConfig,
-  Spotify: createSpotifyOAuthConfig,
-  Twitter: createTwitterOAuthConfig,
-}[provider];
-
-if (createOAuthConfigFn === undefined) {
-  throw new Error("Provider not found");
-}
-
-const redirectUri = Deno.env.get("DENO_DEPLOYMENT_ID") ??
-  "http://localhost:8000/callback";
-const scope = Deno.env.get("SCOPE")!;
-if (!Deno.env.has("GITHUB_CLIENT_ID")) {
-  Deno.env.set("GITHUB_CLIENT_ID", crypto.randomUUID());
-}
-if (!Deno.env.has("GITHUB_CLIENT_SECRET")) {
-  Deno.env.set("GITHUB_CLIENT_SECRET", crypto.randomUUID());
-}
-const oauthConfig = createOAuthConfigFn({ redirectUri, scope });
+const oauthConfig = createGitHubOAuthConfig();
 
 async function indexHandler(request: Request) {
   const sessionId = getSessionId(request);
@@ -79,7 +34,8 @@ async function indexHandler(request: Request) {
     ? `<span style="filter:blur(3px)">${accessToken}</span> (intentionally blurred for security)`
     : accessToken;
   const body = `
-    <p>Provider: ${provider}</p>
+    <p>Authorization endpoint URI: ${oauthConfig.authorizationEndpointUri}</p>
+    <p>Token URI: ${oauthConfig.tokenUri}</p>
     <p>Scope: ${oauthConfig.defaults?.scope}</p>
     <p>Signed in: ${hasSessionIdCookie}</p>
     <p>Your access token: ${accessTokenInnerText}</p>
