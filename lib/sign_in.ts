64663725dd26de18de1706f70ce87cae50c14795
Asher Gomez <ashersaupingomez@gmail.com>
2023-09-21 18:23:23 +1000

    refactor: export `SignInOptions` interface

--- lib/sign_in.ts
+++ lib/sign_in.ts
@@ -10,6 +10,11 @@ import {
   setOAuthSession,
 } from "./_core.ts";
 
+export interface SignInOptions {
+  /** URL parameters that are appended to the authorization URI, if defined. */
+  urlParams?: Record<string, string>;
+}
+
 /**
  * Handles the sign-in process for the given OAuth configuration and redirects
  * the client to the authorization URL.
@@ -25,9 +30,7 @@ import {
  *
  * See "Redirect URL after Sign-In or Sign-Out" section in the README for more
  * information on the success URL.
- *
- * @param request The HTTP request from the client.
- *
+ * *
  * @example
  * ```ts
  * import { signIn, createGitHubOAuthConfig } from "https://deno.land/x/deno_kv_oauth@$VERSION/mod.ts";
@@ -40,13 +43,11 @@ import {
  * ```
  */
 export async function signIn(
+  /** HTTP request from the client */
   request: Request,
   /** @see {@linkcode OAuth2ClientConfig} */
   oauthConfig: OAuth2ClientConfig,
-  options?: {
-    /** These parameters will be appended to the authorization URI, if defined. */
-    urlParams?: Record<string, string>;
-  },
+  options?: SignInOptions,
 ): Promise<Response> {
   const state = crypto.randomUUID();
   const { uri, codeVerifier } = await new OAuth2Client(oauthConfig)
