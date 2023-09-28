a883c5977430b9073c30deb1a916f0613330f65f
iuioiua <iuioiua@users.noreply.github.com>
2023-09-28 00:09:35 +0000

    [create-pull-request] automated change

--- deps.ts
+++ deps.ts
@@ -1,14 +1,14 @@
 // Copyright 2023 the Deno authors. All rights reserved. MIT license.
-export { Status } from "https://deno.land/std@0.202.0/http/http_status.ts";
+export { Status } from "https://deno.land/std@0.203.0/http/http_status.ts";
 export {
   type Cookie,
   deleteCookie,
   getCookies,
   getSetCookies,
   setCookie,
-} from "https://deno.land/std@0.202.0/http/cookie.ts";
+} from "https://deno.land/std@0.203.0/http/cookie.ts";
 export {
   OAuth2Client,
   type OAuth2ClientConfig,
 } from "https://deno.land/x/oauth2_client@v1.0.2/mod.ts";
-export { SECOND } from "https://deno.land/std@0.202.0/datetime/constants.ts";
+export { SECOND } from "https://deno.land/std@0.203.0/datetime/constants.ts";
