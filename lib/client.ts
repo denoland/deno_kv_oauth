63615caab053a79637fcda83731af3f172dcd3e9
Asher Gomez <ashersaupingomez@gmail.com>
2023-10-04 11:14:56 +1100

    copyright

--- lib/client.ts
+++ lib/client.ts
@@ -1,3 +1,4 @@
+// Copyright 2023 the Deno authors. All rights reserved. MIT license.
 import { signIn, type SignInOptions } from "./sign_in.ts";
 import { handleCallback } from "./handle_callback.ts";
 import { signOut } from "./sign_out.ts";
