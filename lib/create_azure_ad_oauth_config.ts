4c91cc445c49b46ae3356577e182ecf7582f3a40
Michael Gearhardt <michael.gearhardt@fathym.com>
2024-02-04 22:02:50 -0700

    feat: `createAzureAdOAuthConfig()` and `createAzureAdb2cOAuthConfig()` (#293)
    * Working azure oauth
    
    * Formatting
    
    * Support for cloud instance without trailing `/`
    
    * Formatting
    
    * working login for adb2c
    
    * Formatting
    
    * handle string scope case
    
    * openid logic around adding app scope
    
    * revert formatting changes
    
    * fix formatting
    
    * Remove commented line
    
    * remove a helper to keep inline with other impls
    
    * formatting
    
    * adjusted to hard code env variables and add provider for ADB2C
    
    * Some adjustments based on comments
    
    * Added tests and got inline with other impls
    
    * Formatting
    
    * remove client logic
    
    * formatting
    
    * fix readme
    
    * Fixed examples
    
    * Fixed examples
    
    * change casing
    
    * Fix example on Azure AD
    
    * revert demo
    
    * Formatting

---
+++ lib/create_azure_ad_oauth_config.ts
@@ -0,0 +1,48 @@
+// Copyright 2023-2024 the Deno authors. All rights reserved. MIT license.
+import type { OAuth2ClientConfig } from "../deps.ts";
+import { getRequiredEnv } from "./get_required_env.ts";
+
+/**
+ * Returns the OAuth configuration for Azure.
+ *
+ * Requires `--allow-env[=AZURE_AD_CLIENT_ID,AZURE_AD_CLIENT_SECRET,AZURE_AD_TENANT_ID]`
+ * permissions and environment variables:
+ * 1. `AZURE_AD_CLIENT_ID`
+ * 2. `AZURE_AD_CLIENT_SECRET`
+ * 4. `AZURE_AD_TENANT_ID`
+ *
+ * @example
+ * ```ts
+ * import { createAzureAdOAuthConfig } from "https://deno.land/x/deno_kv_oauth@$VERSION/mod.ts";
+ *
+ * const oauthConfig = createAzureAdOAuthConfig({
+ *   redirectUri: "http://localhost:8000/callback",
+ *   scope: ["openid"]
+ * });
+ * ```
+ *
+ * @see {@link https://learn.microsoft.com/en-us/entra/identity-platform/v2-protocols-oidc}
+ */
+export function createAzureAdOAuthConfig(config: {
+  /** @see {@linkcode OAuth2ClientConfig.redirectUri} */
+  redirectUri?: string;
+  /** @see {@linkcode OAuth2ClientConfig.defaults.scope} */
+  scope: string | string[];
+}): OAuth2ClientConfig {
+  const baseUrl = `https://login.microsoftonline.com/${
+    getRequiredEnv(
+      "AZURE_AD_TENANT_ID",
+    )
+  }/oauth2/v2.0`;
+
+  return {
+    clientId: getRequiredEnv("AZURE_AD_CLIENT_ID"),
+    clientSecret: getRequiredEnv("AZURE_AD_CLIENT_SECRET"),
+    authorizationEndpointUri: `${baseUrl}/authorize`,
+    tokenUri: `${baseUrl}/token`,
+    redirectUri: config.redirectUri,
+    defaults: {
+      scope: config.scope,
+    },
+  };
+}
---
+++ lib/create_azure_adb2c_oauth_config.ts
@@ -0,0 +1,54 @@
+// Copyright 2023-2024 the Deno authors. All rights reserved. MIT license.
+import type { OAuth2ClientConfig } from "../deps.ts";
+import { getRequiredEnv } from "./get_required_env.ts";
+
+/**
+ * Returns the OAuth configuration for Azure.
+ *
+ * Requires `--allow-env[=AZURE_ADB2C_CLIENT_ID,AZURE_ADB2C_CLIENT_SECRET,AZURE_ADB2C_DOMAIN,AZURE_ADB2C_POLICY,AZURE_ADB2C_TENANT_ID]`
+ * permissions and environment variables:
+ * 1. `AZURE_ADB2C_CLIENT_ID`
+ * 2. `AZURE_ADB2C_CLIENT_SECRET`
+ * 3. `AZURE_ADB2C_DOMAIN`
+ * 4. `AZURE_ADB2C_POLICY`
+ * 5. `AZURE_ADB2C_TENANT_ID`
+ *
+ * @example
+ * ```ts
+ * import { createAzureAdb2cOAuthConfig } from "https://deno.land/x/deno_kv_oauth@$VERSION/mod.ts";
+ *
+ * const oauthConfig = createAzureAdb2cOAuthConfig({
+ *   redirectUri: "http://localhost:8000/callback",
+ *   scope: ["openid", Deno.env.get("AZURE_ADB2C_CLIENT_ID")!]
+ * });
+ * ```
+ *
+ * @see {@link https://learn.microsoft.com/en-us/entra/identity-platform/v2-protocols-oidc}
+ */
+export function createAzureAdb2cOAuthConfig(config: {
+  /** @see {@linkcode OAuth2ClientConfig.redirectUri} */
+  redirectUri?: string;
+  /** @see {@linkcode OAuth2ClientConfig.defaults.scope} */
+  scope: string | string[];
+}): OAuth2ClientConfig {
+  const baseUrl = `https://${
+    getRequiredEnv(
+      "AZURE_ADB2C_DOMAIN",
+    )
+  }/${getRequiredEnv("AZURE_ADB2C_TENANT_ID")}/${
+    getRequiredEnv(
+      "AZURE_ADB2C_POLICY",
+    )
+  }/oauth2/v2.0`;
+
+  return {
+    clientId: getRequiredEnv("AZURE_ADB2C_CLIENT_ID"),
+    clientSecret: getRequiredEnv("AZURE_ADB2C_CLIENT_SECRET"),
+    authorizationEndpointUri: `${baseUrl}/authorize`,
+    tokenUri: `${baseUrl}/token`,
+    redirectUri: config.redirectUri,
+    defaults: {
+      scope: config.scope,
+    },
+  };
+}
