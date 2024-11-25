189ba555bb5efd0c14e93c0cbdc97c9ea4f309e6
Asher Gomez <ashersaupingomez@gmail.com>
2024-11-26 08:23:43 +1100

    refactor: use `jsr:@cmd-johnson/oauth2-client`

--- lib/types.ts
+++ lib/types.ts
@@ -1,6 +1,3 @@
 // Copyright 2023-2024 the Deno authors. All rights reserved. MIT license.
 export type { Cookie } from "@std/http";
-export type {
-  OAuth2ClientConfig,
-  Tokens,
-} from "https://deno.land/x/oauth2_client@v1.0.2/mod.ts";
+export type { OAuth2ClientConfig, Tokens } from "@cmd-johnson/oauth2-client";
